e19748121bbb0181b8bcb1ec8f166e325fb8c98b
vz8946 <weifengli2015@qq.com>
2015-05-22 17:06:57 +0800

    change systemRoot in index.php

--- www/index.php
+++ www/index.php
@@ -3,7 +3,7 @@
  * 引导文件
  */
 //echo "<pre>";
-//var_dump($_SERVER);die;
+//print_r($_SERVER);die;
 @header('Content-type:text/html;charset=UTF-8');
 error_reporting(E_ALL ^ E_NOTICE);
 @ini_set('memory_limit', '128M');
@@ -11,10 +11,11 @@ error_reporting(E_ALL ^ E_NOTICE);
 @ini_set('display_errors', 1);
 @set_magic_quotes_runtime(0);
 date_default_timezone_set('Asia/Shanghai');
-set_include_path(dirname($_SERVER['DOCUMENT_ROOT']) . DIRECTORY_SEPARATOR . 'lib' . PATH_SEPARATOR . dirname($_SERVER['DOCUMENT_ROOT']) . DIRECTORY_SEPARATOR . 'app' .PATH_SEPARATOR . get_include_path());
+$systemRoot = dirname($_SERVER['DOCUMENT_ROOT'] . $_SERVER['REQUEST_URI']);
+set_include_path($systemRoot . DIRECTORY_SEPARATOR . 'lib' . PATH_SEPARATOR . $systemRoot . DIRECTORY_SEPARATOR . 'app' .PATH_SEPARATOR . get_include_path());
 $_SERVER['REMOTE_ADDR'] = isset($_SERVER['HTTP_X_REAL_IP']) ? $_SERVER['HTTP_X_REAL_IP'] : $_SERVER['REMOTE_ADDR'];
 if(!empty($_SERVER["REDIRECT_URL"]))$_SERVER["REQUEST_URI"]=$_SERVER["REDIRECT_URL"];
-$systemRoot = dirname($_SERVER['DOCUMENT_ROOT']);
+//$systemRoot = dirname($_SERVER['DOCUMENT_ROOT']);
 define('SYSROOT', $systemRoot);
 define('SHOP_TPL_ROOT', $systemRoot.'/app/Shop/Views/scripts/');
 define('ADMIN_TPL_ROOT', $systemRoot.'/app/Admin/Views/scripts/');
